65b57fd7b893c9b3f2cb450d80fe991aee2197fd
unknown <amnabaei@ISLOAN15.bcliquor.com>
2015-12-02 16:45:25 -0800

    DS

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -16,16 +16,16 @@ class DriversController < ApplicationController
       #  @zip_code = ZipCode.initialize(params[:driver].to_s)
          @as = Driver.last.first_name
          @pl = 90210
-    # client = Savon.client(wsdl: "http://www.webservicex.net/uszip.asmx?WSDL")
-    # response = client.call(:get_info_by_zip,web: message: { "USZip" => @as })
+    client = Savon.client(wsdl: "http://www.webservicex.net/uszip.asmx?WSDL")
+    response = client.call(:get_info_by_zip, message: { "USZip" => @as })
     #  client = Savon.client(wsdl: "http://www.webservicex.net/airport.asmx?WSDL")
     # response = client.call(:get_airport_information_by_country, message: { "country" => @as })
-     client = Savon.client(wsdl: "http://esb.dev.bcldb.com:25011/LDB_Retail/ProductService?WSDL")
-    
-    response = client.call(:get_product_by_sku, message: { "sku" => @as })
+    #  client = Savon.client(wsdl: "http://esb.dev.bcldb.com:25011/LDB_Retail/ProductService?WSDL") 
+    # response = client.call(:get_product_by_sku, message: { "sku" => @as })
        @res = response
      if response.success?
        data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first
+       @data = response.to_array
        if data
           @state = data[:state]
           @city = data[:city]
