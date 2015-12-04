bcd8809cbd9a1bdad65caac05d6b4aee5240e389
unknown <amnabaei@ISLOAN15.bcliquor.com>
2015-12-04 11:22:03 -0800

    adding correct calling soap service

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -15,27 +15,47 @@ class DriversController < ApplicationController
 
       #  @zip_code = ZipCode.initialize(params[:driver].to_s)
          @as = Driver.last.first_name
-         @pl = 90210
-    client = Savon.client(wsdl: "http://www.webservicex.net/uszip.asmx?WSDL")
-    response = client.call(:get_info_by_zip, message: { "USZip" => @as })
+   # client = Savon.client(wsdl: "http://www.webservicex.net/uszip.asmx?WSDL")
+ ############################# ############################# ############################# ######################################
+ ################################  SETIGN UP SAVON CLIENT AND THEN MAKE A REQUEST TO INVOKE ####################################### 
+ ############################# ############################# #############################  ##################################### 
+     client = Savon.client(wsdl: "http://esb.dev.bcldb.com:25011/LDB_Retail/ProductService?WSDL") 
+
+   ## BELOW I USE SOAP UI READY CODE TO MAKE A REQUEST ##
+    rest = %q(
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:prod="http://productservice.services.bcldb.com">
+           <soapenv:Header/>
+              <soapenv:Body>
+                <prod:productBySKURequest>
+                   <prod:sku>)+@as.to_s+%q(</prod:sku>
+                </prod:productBySKURequest>
+             </soapenv:Body>
+           </soapenv:Envelope>
+            )
+    response = client.call(:get_product_by_sku, xml: rest)
     #  client = Savon.client(wsdl: "http://www.webservicex.net/airport.asmx?WSDL")
     # response = client.call(:get_airport_information_by_country, message: { "country" => @as })
     #  client = Savon.client(wsdl: "http://esb.dev.bcldb.com:25011/LDB_Retail/ProductService?WSDL") 
     # response = client.call(:get_product_by_sku, message: { "sku" => @as })
+
+ #############################  ENDING HITING THE SOAP  SERVICE ####################################### 
+ ############################## ############################## ######################################## 
+
        @res = response
-     if response.success?
-       data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first
+         if response.success?
+           @data = response.to_array
+     #########(only for zip code call) This part is to split response into parts that we want ###### ############################## ##############################
+       data = response.to_array(:get_info_by_zip_response, :get_info_by_zip_result, :new_data_set, :table).first # here we dig into nested hash response to zip code 
        @data = response.to_array
        if data
           @state = data[:state]
           @city = data[:city]
           @area_code = data[:area_code]
           @time_zone = data[:time_zone]
-        
         end
-      end
-      
+      ############################## ############################## ############################## ##############################
 
+         end
 
     	render "drivers/index" # driver index 
       else
